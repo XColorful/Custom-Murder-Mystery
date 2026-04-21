6d3eb43c53ca296b7f8845db53d168076528c4c9
XiaoColorful <XColor_ful@outlook.com>
2026-04-21 10:52:51 +0800

    Update MurderMysteryNeoforge.java

--- neoforge-compat/src/main/java/xiao/murdermystery/compat/neoforge/MurderMysteryNeoforge.java
+++ neoforge-compat/src/main/java/xiao/murdermystery/compat/neoforge/MurderMysteryNeoforge.java
@@ -1,6 +1,7 @@
 package xiao.murdermystery.compat.neoforge;
 
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLLoader;
 import xiao.battleroyale.api.common.McSide;
@@ -9,7 +10,7 @@ import xiao.murdermystery.MurderMystery;
 @Mod(MurderMystery.MOD_ID)
 public class MurderMysteryNeoforge {
 
-    public MurderMysteryNeoforge() {
+    public MurderMysteryNeoforge(IEventBus modEventBus) {
         Dist dist = FMLLoader.getDist();
         McSide mcSide = dist.isClient() ? McSide.CLIENT : McSide.DEDICATED_SERVER;
 
